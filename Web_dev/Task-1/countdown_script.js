e105547b0c9367229e5e369d648626d017057579
Nandhini1998 <nandhini.devi1998@gmail.com>
2016-05-23 12:02:23 +0530

    End of basic mode.Version 2.

--- Web_dev/Task-1/countdown_script.js
+++ Web_dev/Task-1/countdown_script.js
@@ -1,3 +1,4 @@
+
 function getTimeRemaining(endtime){
   var t = Date.parse(endtime) - Date.parse(new Date());
   var seconds = Math.floor( (t/1000) % 60 );
@@ -12,18 +13,20 @@ function getTimeRemaining(endtime){
     'seconds': seconds
   };
 }
+
 window.onload=function()
 {
 var target_date= 'May 31 2016 23:59:59 GMT+05:30';
+document.getElementById("endtime").innerHTML=target_date;
 var time_left=getTimeRemaining(target_date);
 var day_count=time_left.days;
 var hours_count=time_left.hours;
 var min_count=time_left.minutes;
 var sec_count=time_left.seconds;
-document.getElementById("day").innerHTML=day_count;
-document.getElementById("hours").innerHTML=hours_count;
-document.getElementById("min").innerHTML=min_count;
-document.getElementById("sec").innerHTML=sec_count;
+document.getElementById("day").innerHTML=day_count<10?'0'+day_count:day_count;
+document.getElementById("hours").innerHTML=hours_count<10?'0'+hours_count:hours_count;
+document.getElementById("min").innerHTML=min_count<10?'0'+min_count:min_count;
+document.getElementById("sec").innerHTML=sec_count<10?'0'+sec_count:sec_count;
 setInterval(function()
 {
 if(day_count>=0||hours_count>=0||min_count>=0 || sec_count>=0)
@@ -51,11 +54,10 @@ if(day_count>=0||hours_count>=0||min_count>=0 || sec_count>=0)
 	hours_count=0;
 	sec_count=0;
 	}
-	document.getElementById("day").innerHTML=day_count;
-	document.getElementById("hours").innerHTML=hours_count;
-	document.getElementById("min").innerHTML=min_count;
-	document.getElementById("sec").innerHTML=sec_count;
-
+	document.getElementById("day").innerHTML=day_count<10?'0'+day_count:day_count;
+document.getElementById("hours").innerHTML=hours_count<10?'0'+hours_count:hours_count;
+document.getElementById("min").innerHTML=min_count<10?'0'+min_count:min_count;
+document.getElementById("sec").innerHTML=sec_count<10?'0'+sec_count:sec_count;
 }
 },1000);
 };
